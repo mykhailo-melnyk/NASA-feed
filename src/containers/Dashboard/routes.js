0dfeb7f56a9ef1a3189c95d8dbc40ecb5ef3835a
Michael Melnyk <mihael.melnyk@gmail.com>
2017-10-04 17:00:46 +0300

    add process.env.BASENAME

--- src/containers/Dashboard/routes.js
+++ src/containers/Dashboard/routes.js
@@ -1,6 +1,6 @@
 import Home from '../Home';
 import { RouterGenerator } from '../../services';
 
-export const routes = new RouterGenerator('/')
+export const routes = new RouterGenerator(process.env.BASENAME)
   .addRoute('/', Home, true)
   .getRoutes();
